23a75887adca832836ba549e660a69a515cef0a7
Fausto Llapa <faustollapa850@gmail.com>
2020-08-05 15:47:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  # Your code here
+  planeteer_calls.any? do |call|
+  call.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
