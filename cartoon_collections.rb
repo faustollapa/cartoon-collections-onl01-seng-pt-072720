c1c92a82cee58e858d9a8b42360a211e2950748c
Fausto Llapa <faustollapa850@gmail.com>
2020-08-05 15:49:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,8 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_the_cheese# code an argument here
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find do |item|
+    cheese_types.include?(item)
+  end
 end
